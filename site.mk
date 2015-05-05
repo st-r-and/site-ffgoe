ba1b9acd9f841253403459906e947616f5398a79
alexander eickhoff <alex@artbanause.de>
2015-05-05 09:38:36 +0200

    version +1

--- site.mk
+++ site.mk
@@ -27,7 +27,7 @@ GLUON_SITE_PACKAGES := \
 	iperf \
 	tcpdump-mini
 
-DEFAULT_GLUON_RELEASE := 0.7_ae17
+DEFAULT_GLUON_RELEASE := 0.7_ae18
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
