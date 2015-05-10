1af32d9936542fb53e376b266b3d9cee257fcb35
alexander eickhoff <alex@artbanause.de>
2015-05-10 14:04:18 +0200

    reorder site.mk

--- site.mk
+++ site.mk
@@ -4,28 +4,27 @@ GLUON_SITE_PACKAGES := \
 	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
-	gluon-next-node \
 	gluon-mesh-vpn-fastd \
+	gluon-next-node \
 	gluon-radvd \
-	gluon-status-page \
 	gluon-setup-mode \
-	gluon-config-mode-core \
-	iwinfo \
-	iptables \
+	gluon-status-page \
 	haveged \
-	uclibcxx \
 	iperf \
-	tcpdump-mini
+	iptables \
+	iwinfo \
+	tcpdump-mini \
+	uclibcxx
 
 DEFAULT_GLUON_RELEASE := 0.7_ae21
 
