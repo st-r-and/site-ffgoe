576796c707de25845d56a48a4d6862d0e2c6e9ac
alexander eickhoff <alex@artbanause.de>
2015-07-14 18:04:47 +0200

    sync to 0.8

--- site.mk
+++ site.mk
@@ -11,22 +11,21 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
-	gluon-mesh-vpn-fastd \
+	gluon-luci-wifi-config \
 	gluon-next-node \
+	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
-	haveged \
-	iperf \
-	iptables \
 	iwinfo \
-	tcpdump-mini \
-	uclibcxx
+	iptables \
+	haveged
 
-DEFAULT_GLUON_RELEASE := 0.7_ae50
+DEFAULT_GLUON_RELEASE := 0.8_ae01
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
@@ -34,4 +33,4 @@ GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 GLUON_PRIORITY ?= 0
 
 # Languages to include
-GLUON_LANGS ?= de
+GLUON_LANGS ?= de en
