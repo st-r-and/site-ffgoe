ddfe5c90aa38c6a2055b6ed26bfdcb4ea2f8e3ad
alexander eickhoff <alex@artbanause.de>
2015-07-23 18:04:42 +0200

    version +1

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.8_ae02
+DEFAULT_GLUON_RELEASE := 0.8_ae03
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
