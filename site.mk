116c9ac0164148053759d377dda0e4280dfcb818
alexander eickhoff <alex@artbanause.de>
2015-12-03 16:59:29 +0100

    + usb for x86

--- site.mk
+++ site.mk
@@ -23,6 +23,44 @@ GLUON_SITE_PACKAGES := \
 	iwinfo \
 	iptables \
 	haveged
+
+ifeq ($(GLUON_TARGET),x86-generic)
+# support the usb stack on x86 devices
+# and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+	kmod-mii \
+	kmod-usb2 \
+	kmod-usb-core \
+	kmod-usb-hid \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-eem \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-cdc-mbim \
+	kmod-usb-net-cdc-ncm \
+	kmod-usb-net-cdc-subset \
+	kmod-usb-net-dm9601-ether \
+	kmod-usb-net-hso \
+	kmod-usb-net-huawei-cdc-ncm \
+	kmod-usb-net-ipheth \
+	kmod-usb-net-kalmia \
+	kmod-usb-net-kaweth \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-qmi-wwan \
+	kmod-usb-net-rndis \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-sierrawireless \
+	kmod-usb-net-smsc95xx \
+	joe \
+	tcpdump \
+	vnstat \
+	iperf \
+	usbutils
+endif
+
 	
 DEFAULT_GLUON_RELEASE := m_ae_$(shell date '+%Y%m%d%H%M')
 
